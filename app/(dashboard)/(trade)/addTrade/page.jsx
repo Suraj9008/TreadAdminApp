7aecfce5b0e5b893c6103a3fdb4d5f0dc3273436
suraj.narule <suraj.narule@embarkingonvoayge.com>
2023-11-05 15:14:41 +0530

    add new pages

--- app/(dashboard)/(trade)/addTrade/page.jsx
+++ app/(dashboard)/(trade)/addTrade/page.jsx
@@ -1,21 +1,23 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Card from "@/components/ui/Card";
 import Textinput from "@/components/ui/Textinput";
-import Textarea from "@/components/ui/Textarea";
 import Select from "@/components/ui/Select";
 import Flatpickr from "react-flatpickr";
 import ExampleOne from "@/components/partials/table/ExampleOne";
+import Fileinput from "@/components/ui/Fileinput";
+import Button from "@/components/ui/Button";
+
 const AddTradePage = () => {
+  const [basic, setBasic] = useState(new Date());
   return (
     <>
-      <h1 className="text-center">Trade Data Page</h1>
       <div className="pt-5">
-        <Card title="Basic Inputs">
+        <Card title="Trade Data Page">
           <div className="">
             <div className="space-y-3">
               <Textinput
-                label="Project Name*"
+                label="Trade Id*"
                 id="pn"
                 type="text"
                 placeholder="Management dashboard "
@@ -23,7 +25,7 @@ const AddTradePage = () => {
             </div>
             <div className="space-y-3">
               <Textinput
-                label="Project Name*"
+                label="Note*"
                 id="pn"
                 type="text"
                 placeholder="Management dashboard "
@@ -32,12 +34,12 @@ const AddTradePage = () => {
             <div className="space-y-3">
               <Select
                 options={["Option 1", "Option 2", "Option 3"]}
-                label="Select Option's"
+                label="User Type"
               />
             </div>
             <div className="space-y-3">
               <Textinput
-                label="Project Name*"
+                label="Company Name*"
                 id="pn"
                 type="text"
                 placeholder="Management dashboard "
@@ -45,40 +47,97 @@ const AddTradePage = () => {
             </div>
             <div className="space-y-3">
               <label htmlFor="default-picker" className=" form-label">
-                Default Functionality
+                Date
               </label>
-
               <Flatpickr
                 className="form-control py-2"
                 placeholder="YYYY-MM-DD"
                 value=""
-                id="default-picker"
-              />
-            </div>
-            <div className="space-y-3">
-              <label htmlFor="default-picker" className=" form-label">
-                Default Functionality
-              </label>
-
-              <Flatpickr
-                className="form-control py-2"
-                value=""
-                placeholder="YYYY-MM-DD"
                 id="default-picker"
               />
             </div>
             <div className="space-y-3">
               <Select
                 options={["Option 1", "Option 2", "Option 3"]}
-                label="Select Option's"
+                label="Status"
+              />
+            </div>
+            <div className="space-y-3">
+              <label className=" form-label">Upload Image</label>
+              <Fileinput name="basic" selectedFile="" />
+            </div>
+            <div className="space-y-3">
+              <Textinput
+                label="Buy Price"
+                id="pn"
+                type="text"
+                placeholder="Management dashboard "
+              />
+            </div>
+            <div className="space-y-3">
+              <Textinput
+                label="SL Price"
+                id="pn"
+                type="text"
+                placeholder="Management dashboard "
+              />
+            </div>
+            <div className="space-y-3">
+              <Textinput
+                label="TGT Price"
+                id="pn"
+                type="text"
+                placeholder="Management dashboard "
+              />
+            </div>
+            <div>
+              <label className="form-label" id="timepicker">
+                Basic 24hrs
+              </label>
+              <Flatpickr
+                className="form-control py-2"
+                value={basic}
+                id="timepicker"
+                options={{
+                  enableTime: true,
+                  noCalendar: true,
+                  dateFormat: "H:i",
+                  time_24hr: true,
+                }}
+                onChange={(date) => setBasic(date)}
+              />
+            </div>
+            <div className="space-y-3">
+              <Textinput
+                label="Result Text View"
+                id="pn"
+                type="text"
+                placeholder="Management dashboard "
               />
             </div>
           </div>
+          <div className="columns-3 mt-5">
+            <Button
+              text="Save"
+              className="btn-outline-dark bg-success-500 text-lg border rounded border-success-500 text-white mr-3"
+            />
+            <Button
+              text="Delete"
+              className="btn-outline-dark bg-danger-500 text-lg border rounded border-danger-500 text-white mx-3"
+            />
+            <Button
+              text="Get"
+              className="btn-outline-dark bg-primary-500 text-lg border rounded border-primary-500 text-white mx-3"
+            />
+            <Button
+              text="Update"
+              className="btn-outline-dark bg-warning-500 text-lg border rounded border-warning-500 text-white mx-3"
+            />
+          </div>
         </Card>
       </div>
-
       <div className="mt-5">
-      <ExampleOne />
+        <ExampleOne />
       </div>
     </>
   );
--- app/(dashboard)/(trade)/addTradeAction/page.jsx
+++ app/(dashboard)/(trade)/addTradeAction/page.jsx
@@ -1,7 +1,133 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Card from "@/components/ui/Card";
+import Textinput from "@/components/ui/Textinput";
+import Flatpickr from "react-flatpickr";
+import ExampleOne from "@/components/partials/table/ExampleOne";
+import Button from "@/components/ui/Button";
 
 const AddTradeActionPage = () => {
-  return <div>Add Trade Action</div>;
+  const [basic, setBasic] = useState(new Date());
+  return (
+    <div className="pt-5">
+      <Card title="Trade Action Update">
+        <div className="flex justify-between">
+          <div className="mt-10">
+            <div className="">
+              <div className="w-50">
+                <div className="flex">
+                  <div className="space-y-3">
+                    <Textinput
+                      label="Id*"
+                      id="pn"
+                      type="text"
+                      placeholder="Id"
+                    />
+                  </div>
+                  <div className="space-y-3">
+                    <Textinput
+                      label="Trade Id*"
+                      id="pn"
+                      type="text"
+                      placeholder="Trade Id"
+                    />
+                  </div>
+                </div>
+              </div>
+              <dev className="">
+                <Button
+                  text="Get Data"
+                  className="btn-outline-dark btn-sm bg-success-500 text-lg border rounded border-success-500 text-white mt-3 text-center ml-44"
+                />
+              </dev>
+            </div>
+            <div className="flex mt-3">
+              <div>
+                <label htmlFor="default-picker" className=" form-label">
+                  Date
+                </label>
+                <Flatpickr
+                  className="form-control py-2"
+                  placeholder="YYYY-MM-DD"
+                  value=""
+                  id="default-picker"
+                />
+              </div>
+              <div>
+                <label className="form-label" id="timepicker">
+                  Time
+                </label>
+                <Flatpickr
+                  className="form-control py-2"
+                  value={basic}
+                  id="timepicker"
+                  options={{
+                    enableTime: true,
+                    noCalendar: true,
+                    dateFormat: "H:i",
+                    time_24hr: true,
+                  }}
+                  onChange={(date) => setBasic(date)}
+                />
+              </div>
+            </div>
+            <div className="flex mt-3">
+              <div className="">
+                <div className="grid flex-row">
+                  <Textinput
+                    label="Current Price"
+                    id="pn"
+                    type="text"
+                    placeholder="Current Price"
+                  />
+                  <Textinput
+                    label="SL Price"
+                    id="pn"
+                    type="text"
+                    placeholder="SL Price"
+                  />
+                  <Textinput
+                    label="TGT Price"
+                    id="pn"
+                    type="text"
+                    placeholder="TGT Price"
+                  />
+                </div>
+              </div>
+              <div className="space-y-3 flex items-center">
+                <Textinput
+                  label="Action"
+                  id="pn"
+                  type="text"
+                  placeholder="Action"
+                />
+              </div>
+            </div>
+            <div className="flex mt-5">
+              <Button
+                text="Save"
+                className="btn-outline-dark bg-success-500 text-lg border rounded border-success-500 text-white mr-3"
+              />
+              <Button
+                text="Delete"
+                className="btn-outline-dark bg-danger-500 text-lg border rounded border-danger-500 text-white mx-3"
+              />
+              <Button
+                text="Get"
+                className="btn-outline-dark bg-primary-500 text-lg border rounded border-primary-500 text-white mx-3"
+              />
+              <Button
+                text="Update"
+                className="btn-outline-dark bg-warning-500 text-lg border rounded border-warning-500 text-white mx-3"
+              />
+            </div>
+          </div>
+          <div className="ml-5">
+            <ExampleOne />
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
 };
-
 export default AddTradeActionPage;
